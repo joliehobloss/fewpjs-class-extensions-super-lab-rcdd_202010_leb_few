641e6c6216e67e096b08658144614ffc1747a31f
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-13 15:00:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ class Deciduous extends Tree{
   }
   
    static definition(){
-    return super.definition() + '  Deciduous trees shed their leaves annually.';
+    return super.definition() + ' Deciduous trees shed their leaves annually.';
   }
 }
 

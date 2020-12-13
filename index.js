fbe13c794b953503538e3ca4915fa5172a4256ca
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-13 14:51:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ class Deciduous extends Tree{
   }
   
     static definition(){
-    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.";
+    return super.logNbSides() + ' which are all equal';
   }
 }
 

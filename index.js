0af293b89a380898007d7a4fede96df45f0a0e46
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-13 15:02:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,23 +33,3 @@ class Evergreen extends Tree{
     return super.definition() + ' Evergreens keep their leaves all year round.';
   }
 }
-/*describe( "Tree", () => {
-  let tree
-
-describe( "Evergreen", () => {
-  let fir
-
-  it( 'initializes with two parameters, species and name, and uses `super` to set `species`', () => {
-    fir = new Evergreen( 'Fir', 'Douglas' )
-    expect( fir.species )
-      .to.eql( 'Fir' )
-    expect( fir.name )
-      .to.eql( 'Douglas' )
-  } )
-
-  it( 'has a static method `definition` that uses `super.definition` and expands on it', () => {
-    expect( Evergreen.definition() )
-      .to.eql( "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves. Evergreens keep their leaves all year round." )
-  } )
-
-} )*/
